bd4201738ca72bc9d14ccbc81dff17f5ca4ad24c
Timbittt <157414989+Timbittt@users.noreply.github.com>
2024-02-09 13:12:18 -0500

    Added header and input

--- fahr_to_cel.py
+++ fahr_to_cel.py
@@ -1,1 +1,9 @@
+#NAME:  Timofiy Kharchuk
 
+#STUDENT NUMBER:  1008424
+
+#ICS3U0 SECTION:  E
+
+#OVERVIEW:  Converts inputted Fahrenheit temperature and outputs it in Celsius
+
+FahrenheitTemperature = float(input("Enter temperature to be converted and press enter: "))
