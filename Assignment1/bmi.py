b12713d444d2be69910961eddca7785c167be2cd
Timbittt <157414989+Timbittt@users.noreply.github.com>
2024-02-09 13:46:30 -0500

    debugging, fixed errors, and rounded output

--- Assignment1/bmi.py
+++ Assignment1/bmi.py
@@ -9,20 +9,19 @@
 # get type of units from user
 units = input("Enter type of units (M or Metric for Metric, I or Imperial for Imperial), and press enter:")
 
-# prompt and get input based off of system
+# prompt and get input based off of system, then calculate BMI
 if units == "M" or units == "Metric":
-    height = input("Enter your height in centimeters and press enter:")
-    weight = input("Enter your weight in kilograms and press enter:")
+    height = int(input("Enter your height in centimeters and press enter:"))
+    weight = int(input("Enter your weight in kilograms and press enter:"))
+    bmi = weight / (height/100)**2
 
 elif units == "I" or units == "Imperial":
-    height = input("Enter your height in inches and press enter:")
-    weight = input("Enter your weight in pounds and press enter:")
+    height = int(input("Enter your height in inches and press enter:"))
+    weight = int(input("Enter your weight in pounds and press enter:"))
+    bmi = 703 * (weight / height**2)
 
 else:
     print("ERROR: invalid input, restart program and enter a valid input")
 
-# calculate bmi
-bmi = weight / height**2
-
-# output result
-print("Based on the given measurements, your BMI is", bmi)
+# output rounded result
+print("Based on the given measurements, your BMI is", round(bmi, 2))
