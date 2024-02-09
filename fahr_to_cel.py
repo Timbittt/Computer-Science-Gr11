a77975848e984c77d8bb8536da4fe4d5a7f163c9
Timbittt <157414989+Timbittt@users.noreply.github.com>
2024-02-09 13:17:26 -0500

    Added header and i

--- fahr_to_cel.py
+++ fahr_to_cel.py
@@ -6,4 +6,11 @@
 
 #OVERVIEW:  Converts inputted Fahrenheit temperature and outputs it in Celsius
 
-FahrenheitTemperature = float(input("Enter temperature to be converted and press enter: "))
+# Gets input from user
+fahrenheitTemperature = float(input("Enter temperature to be converted and press enter: "))
+
+# Converts input to fahrenheit
+celsiusTemperature = (fahrenheitTemperature - 32) * 5/9
+
+# Returns result
+print(fahrenheitTemperature, "in celsius is:", celsiusTemperature)
