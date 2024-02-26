1df583ff929202f1b70149d5a6990ef943b15fa8
Timofiy Kharchuk <157414989+Timbittt@users.noreply.github.com>
2024-02-26 13:23:43 -0500

    Update IMS.py

--- Assignment 3 - IMS/IMS.py
+++ Assignment 3 - IMS/IMS.py
@@ -8,6 +8,25 @@
 
 print("Welcome to the IMS.")
 
+loop = True
+
+ids = []
+price = []
+desc = []
+stock = []
+
+def changePrice(id, newPrice):
+    price[id] = newPrice
+
+def changeDesc(id, newDesc):
+    desc[id] = newDesc
+
+def changeStock(id, stockAmount):
+    return 0
+
+def changeId(oldId, newId):
+    return 0
+
 def quitProgram():
     print("Quiting... ")
     return False
@@ -15,13 +34,28 @@ def quitProgram():
 def help():
     print("This program is still currently in development.")
 
-loop = True
+def commandNotFound():
+    print("The command entered does not exist, try again or enter h for a list of commands")
+
+def checkInput(command):
+    match command.lower():
+        case 'p':
+            changePrice()
+        case 'd':
+            changeDesc()
+        case 'st':
+            changeStock()
+        case 'id':
+            changeId()
+        case 'q':
+            return quitProgram()
+        case 'h':
+            help()
+        case _:
+            commandNotFound()
+
+    
+    return loop
 
 while loop:
-    command = input("Enter a command: ")
-
-    if command == 'Q' or command == 'q':
-        loop = quitProgram()
-    
-    elif command == 'H' or command == 'h':
-        help()
+    loop = checkInput(input("Enter a command: "))
