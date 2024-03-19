746efa943a817a0fcd7c004274689b4e2c6a0252
Timofiy Kharchuk <157414989+Timbittt@users.noreply.github.com>
2024-03-19 16:27:15 +0000

    created new program

--- Assignment 4 - Functions/quadraticSolver.py
+++ Assignment 4 - Functions/quadraticSolver.py
@@ -1,18 +1,18 @@
-import math
-
 def solveQuadratic(a, b, c):
-    if not(a and b and c):
+    if (not a) and (not b) and (not c):
         return -1, 0.0, 0.0
-    if not(a and b):
+    if (not a) and (not b):
         return -2, 0.0, 0.0
     if not a:
-        return 3, 0.0, 0.0
+        return 3, 2.0, 2.0
 
     root = b**2 - 4*a*c
 
-    if root > 0:
-        return ( 2, ( -b + math.sqrt(root) ) / ( 2*a ), ( -b - math.sqrt(root) ) / ( 2*a ) )
-    elif root == 0:
-        return ( 1, (-b / (2 * a)), (-b / (2 * a)))
+    if root:
+        return ( 2, ( -b + root**0.5 ) / ( 2*a ), ( -b - root**0.5 ) / ( 2*a ))
     else:
-        return (0, 0.0, 0.0)
+        return ( 1, (-b / (2 * a)), (-b / (2 * a)))
+    
+
+while True:
+    print(solveQuadratic(int(input("Enter a: ")), int(input("Enter b: ")), int(input("Enter c: "))))
